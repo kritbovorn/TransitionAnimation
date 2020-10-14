76814ddfa961e515f659f78a76502a4537574fce
kritbovorn <ktaweeyossak@yahoo.com>
2020-10-14 18:29:35 +0700

    Initial Commit

---
+++ TransitionAnimation/TransitionAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransitionAnimationApp.swift
+//  TransitionAnimation
+//
+//  Created by Kritbovorn Taweeyossak on 14/10/2563 BE.
+//
+
+import SwiftUI
+
+@main
+struct TransitionAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
